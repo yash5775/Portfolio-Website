ffc3c9a000b64db05ed5f46baf07524308b05d1f
yash <yash.chaniyara@icloud.com>
2025-12-13 18:29:25 +0530

    refactor: Remove ConnectorLine component from Overlay

--- src/components/ui/Overlay.jsx
+++ src/components/ui/Overlay.jsx
@@ -110,7 +110,6 @@ export default function Overlay() {
 
                     {/* Dynamic Curve Transition */}
                     <Curve />
-                    <ConnectorLine />
 
                     {/* Intro Text Matching Reference Position - Reduced Padding to lift text up */}
                     <div className="w-full flex flex-col items-center justify-center pt-20 pb-10 text-center px-4">
